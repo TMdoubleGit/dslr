6fc79533d7d398e45012e305e8020676e9f987ac
tmichel- <tmichel-@student.42.com>
2024-03-30 19:18:45 +0100

    fnl step1

--- describe.py
+++ describe.py
@@ -6,10 +6,7 @@ def cleanData(dataframe):
     """
     This function replaces the NA values of a dataframe by 0.
     """
-    for col in dataframe.columns:
-        dataframe[col].fillna(int(0), inplace=True)
-    
-    print(dataframe)
+    dataframe.fillna(0, inplace=True)
     return dataframe
 
 
@@ -23,21 +20,20 @@ def describe(path: str):
             dataset = pd.DataFrame(dataset)
 
         numerical_features = dataset.select_dtypes(include=['number'])
-        dataset_to_analyse = dataset[numerical_features.columns]
-        dataset_to_analyse = cleanData(dataset_to_analyse)
+        dataset_to_analyse = cleanData(dataset[numerical_features.columns])
 
         stats = []
         for colonne in dataset_to_analyse.columns:
-            data = dataset[colonne]
+            data = dataset_to_analyse[colonne]
             s_data = sorted(data)
-            count = len(data)
-            mean = data.sum() / count
-            std = math.sqrt((sum(pow(x - mean, 2) for x in s_data) / count))
-            minimum = s_data[0]
-            q25 = s_data[int(0.25 * count)]
-            q50 = s_data[int(count // 2)] if count % 2 == 1 else s_data[int(count // 2 + 1)]
-            q75 = s_data[int(0.75 * count)]
-            maximum = s_data[count - 1]
+            count = round(len(data), 2)
+            mean = round(data.sum() / count, 2)
+            std = round(math.sqrt((sum(pow(x - mean, 2) for x in s_data if not pd.isnull(x)) / count)), 2)
+            minimum = round(s_data[0], 2)
+            q25 = round(s_data[int(0.25 * count)], 2)
+            q50 = round(s_data[int(count / 2)] if count % 2 == 1 else s_data[int(count / 2 + 1)], 2)
+            q75 = round(s_data[int(0.75 * count)], 2)
+            maximum = round(s_data[count - 1], 2)
 
             stats.append([colonne, count, mean, std, minimum, q25, q50, q75, maximum])
             columns = ['Feature', 'Count', 'Mean', 'Std', 'Min', '25%', '50%', '75%', 'Max']
